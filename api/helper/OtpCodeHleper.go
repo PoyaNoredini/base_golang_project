ccaa5d2bdd4c7d52eb0e83a2d54e147370d1f430
poyanoredini <poya.ali.m@gmail.com>
2026-06-15 09:32:09 +0000

    create otp code helper

---
+++ api/helper/OtpCodeHleper.go
@@ -0,0 +1,33 @@
+package helper
+
+
+import (
+	"math/rand"
+	"fmt"
+	"time"
+)
+
+func GenerateOtpCode() string {
+	rand.Seed(time.Now().UnixNano())
+	return fmt.Sprintf("%06d", rand.Intn(1000000))
+}
+
+func VerifyCode(code string, mobile string) bool {
+    var otp models.OtpCode
+
+    result := config.DB.
+        Where("code = ? AND phone_number = ?", code, mobile).
+        Where("created_at >= ?", time.Now().Add(-2*time.Minute)). // expires after 2 min
+        Where("deleted_at IS NULL").
+        First(&otp)
+
+    if result.Error != nil {
+        return false
+    }
+
+    // Soft delete after use — so it can't be reused (like invalidating token)
+    config.DB.Delete(&otp)
+	//
+
+    return true
+}
