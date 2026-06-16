247002c69177ad79c3fcdffd18d34968d77d0234
poyanoredini <poya.ali.m@gmail.com>
2026-06-16 08:44:36 +0000

    create app error handler

---
+++ api/errors/app_error.go
@@ -0,0 +1,37 @@
+package errors
+
+import "net/http"
+
+type AppError struct {
+    Status  int
+    Message string
+}
+
+func (e *AppError) Error() string {
+    return e.Message
+}
+
+func BadRequest(msg string) *AppError  {
+     return &AppError{
+        Status: http.StatusBadRequest,
+        Message: msg,
+     }
+}
+func NotFound(msg string) *AppError    {
+     return &AppError{
+        Status: http.StatusNotFound,
+        Message: msg,
+     }
+}
+func Unauthorized(msg string) *AppError {
+     return &AppError{
+        Status: http.StatusUnauthorized,
+        Message: msg,
+     }
+}
+func Internal(msg string) *AppError    {
+     return &AppError{
+        Status: http.StatusInternalServerError,
+        Message: msg,
+     }
+}
