f2c5780093747466543e140e896f3818d722e574
poyanoredini <poya.ali.m@gmail.com>
2026-06-16 10:15:38 +0000

    update jwt file and add new funciton for parsig token

--- api/helper/jwt.go
+++ api/helper/jwt.go
@@ -3,6 +3,7 @@ package helper
 import (
     "os"
     "time"
+    "errors"
     "github.com/golang-jwt/jwt/v5"
 )
 
@@ -24,4 +25,24 @@ func GenerateToken(userID uint, phone string) (string, error) {
 
     token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
     return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
+
+func ParseToken(tokenStr string) (*Claims, error) {
+    token, err := jwt.ParseWithClaims(tokenStr, &Claims{}, func(t *jwt.Token) (interface{}, error) {
+        if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+            return nil, ErrTokenInvalid
+        }
+        return []byte(os.Getenv("JWT_SECRET")), nil
+    })
+
+    if err != nil {
+        return nil, errors.New("invalid token")
+    }
+
+    claims, ok := token.Claims.(*Claims)
+    if !ok || !token.Valid {
+        return nil, errors.New("invalid token")
+    }
+
+    return claims, nil
 }
