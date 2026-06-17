e50fc05bebff44453d82b9b95f186c0a6671fd6a
poyanoredini <poya.ali.m@gmail.com>
2026-06-17 07:47:44 +0000

    update route file

--- routes/routes.go
+++ routes/routes.go
@@ -1,23 +1,15 @@
 package routes
 
 import (
-	v01 "BaseProject/api/controllers/v01"
-
-	"github.com/gin-gonic/gin"
+    "BaseProject/api/middleware"
+    "github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(r *gin.Engine) {
-	authController := &v01.AuthController{}
+    r.Static("/storage", "./storage")
 
-	api := r.Group("/api/v1")
-	{
-		api.Group("/auth") {
-			aut.POST("/send-otp-code", authController.SendOtpCode)
-			aut.POST("/login-with-otp", authController.LoginWithOtp)
-			aut.POST("/login-with-password", authController.LoginWithPassword)
-			aut.POST("/register", authController.Register)
-		}
-	}
+    api := r.Group("/api/v1")
 
-
+    RegisterAuthRoutes(api)
+    RegisterFileRoutes(api, middleware.Auth())
 }
